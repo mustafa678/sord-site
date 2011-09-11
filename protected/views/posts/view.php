f80ae45f3f4a0135566fa44c682ef69994e9e34a
Mustafa Mohammed <mustafa678@gmail.com>
2011-09-11 15:27:20 +0300

    some little changes to te posts view

--- protected/views/posts/view.php
+++ protected/views/posts/view.php
@@ -6,14 +6,14 @@ $this->breadcrumbs=array(
 
 $this->menu=array(
 	array('label'=>'List Posts', 'url'=>array('index')),
-	array('label'=>'Create Posts', 'url'=>array('create')),
-	array('label'=>'Update Posts', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Posts', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Create Post', 'url'=>array('create')),
+	array('label'=>'Update Post', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete Post', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Posts', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Posts #<?php echo $model->id; ?></h1>
+<h1>View Post: <?php echo $model->title; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
